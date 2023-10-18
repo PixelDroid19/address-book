fe1c5a466b137fb1018966aa21f78743cf1b1cb0
Damien Monasterios <monasterio13septiembre@gmail.com>
2023-10-18 13:16:58 -0500

    Correction minor.

--- src/components/ListAddress.jsx
+++ src/components/ListAddress.jsx
@@ -1,34 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Table from 'react-bootstrap/Table';
-import { apiClient } from '../axios/AddresApi';
-import useForm from '../hook/useForm';
-import ContactItem from './ContactItem';
+import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Table from "react-bootstrap/Table";
+import { apiClient } from "../axios/AddresApi";
+import useForm from "../hook/useForm";
+import ContactItem from "./ContactItem";
 
 const ListAddress = () => {
   const initialState = {
-    nombre: '',
-    apellido: '',
-    telefono: '',
-    direccion: '',
+    nombre: "",
+    apellido: "",
+    telefono: "",
+    direccion: "",
   };
 
-  // Custom hook for form handling
   const { handleInputChange, values, setValues } = useForm({
-    initialState
+    initialState,
   });
 
-  // State for storing contact data
   const [data, setData] = useState([]);
 
   // Function to fetch contact data from the API
   const fetchContactData = async () => {
     try {
-      const response = await apiClient.get('/contactos');
+      const response = await apiClient.get("/contactos");
       if (response.status === 200) {
         setData(response.data);
       } else {
@@ -56,15 +54,19 @@ const ListAddress = () => {
   // Function to create a contact
   const createContact = async () => {
     try {
-      const response = await apiClient.post('/contactos', values);
+      const response = await apiClient.post("/contactos", values);
       if (response.status === 201) {
         setValues(initialState);
         fetchContactData();
       } else {
-        console.error(`Failed to create contact with status: ${response.status}`);
+        console.error(
+          `Failed to create contact with status: ${response.status}`
+        );
       }
     } catch (error) {
-      console.error(`An error occurred while creating the contact: ${error.message}`);
+      console.error(
+        `An error occurred while creating the contact: ${error.message}`
+      );
     }
   };
 
@@ -75,10 +77,14 @@ const ListAddress = () => {
       if (response.status === 200) {
         fetchContactData();
       } else {
-        console.error(`Failed to update contact with status: ${response.status}`);
+        console.error(
+          `Failed to update contact with status: ${response.status}`
+        );
       }
     } catch (error) {
-      console.error(`An error occurred while updating the contact: ${error.message}`);
+      console.error(
+        `An error occurred while updating the contact: ${error.message}`
+      );
     }
   };
 
@@ -88,53 +94,53 @@ const ListAddress = () => {
 
   return (
     <Container fluid>
-      <Row className='roomfac fontReg'>
-        <Col xs={5} className='User'>
-        <Form.Group className='mb-3'>
+      <Row className="roomfac fontReg">
+        <Col xs={5} className="User">
+          <Form.Group className="mb-3">
             <Form.Label>Nombre</Form.Label>
             <Form.Control
-              type='text'
+              type="text"
               value={values?.nombre}
-              name='nombre'
-              placeholder='Nombre'
+              name="nombre"
+              placeholder="Nombre"
               onChange={handleInputChange}
             />
           </Form.Group>
 
-          <Form.Group className='mb-3'>
+          <Form.Group className="mb-3">
             <Form.Label>Apellido</Form.Label>
             <Form.Control
-              type='text'
-              name='apellido'
+              type="text"
+              name="apellido"
               value={values?.apellido}
-              placeholder='Apellido'
+              placeholder="Apellido"
               onChange={handleInputChange}
             />
           </Form.Group>
 
-          <Form.Group className='mb-3'>
+          <Form.Group className="mb-3">
             <Form.Label>Dirección</Form.Label>
             <Form.Control
-              type='text'
+              type="text"
               value={values?.direccion}
-              name='direccion'
-              placeholder='Dirección'
+              name="direccion"
+              placeholder="Dirección"
               onChange={handleInputChange}
             />
           </Form.Group>
 
-          <Form.Group className='mb-3'>
+          <Form.Group className="mb-3">
             <Form.Label>Teléfono</Form.Label>
             <Form.Control
-              type='number'
-              name='telefono'
+              type="number"
+              name="telefono"
               value={values?.telefono}
-              placeholder='Teléfono'
+              placeholder="Teléfono"
               onChange={handleInputChange}
             />
           </Form.Group>
 
-          <Button variant='primary' onClick={createContact}>
+          <Button variant="primary" onClick={createContact}>
             Enviar
           </Button>
         </Col>
